6cd0945b94a308980935749220c8cf7c16d6e8ff
mattfisher <nuggsrocks@yahoo.com>
2021-11-19 18:40:37 -0500

    express setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use((req, res, next) => {
+  res.status(404).type('text').send('404 Not Found')
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => console.log('listening on port ' + PORT))
